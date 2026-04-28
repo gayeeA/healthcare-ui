8e2fa6a1fef274bb67b752ae4c898879129984c5
GAYATHRI A <145894167+gayeeA@users.noreply.github.com>
2026-04-28 14:25:38 +0530

    replaced the API keys

--- src/firebase.ts
+++ src/firebase.ts
@@ -8,13 +8,13 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBSfBTmgs3aXQ8jK7s6STEPNcrS8iHCRh4",
-  authDomain: "healthcare-ui-2cbaa.firebaseapp.com",
-  projectId: "healthcare-ui-2cbaa",
-  storageBucket: "healthcare-ui-2cbaa.firebasestorage.app",
-  messagingSenderId: "392763407321",
-  appId: "1:392763407321:web:61a91c2f3aa024c28b77c8",
-  measurementId: "G-70TCS1D6FX"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
@@ -23,3 +23,4 @@ const analytics = getAnalytics(app);
 export const auth = getAuth(app);
 
 export default app;
+
